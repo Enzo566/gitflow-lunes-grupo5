ca6cc598abd3a73c4a80a9e3fb5172cf291e88c3
Enzo Cortez <enzo.cortez@udla.edu.ec>
2026-06-28 21:16:14 -0500

    Feature: Actualizacion del saludo a diferentes ididiomas

--- Main.java
+++ Main.java
@@ -5,9 +5,11 @@ public class Main {
         System.out.println("***************");
 
         Saludo saludo = new Saludo();
-        System.out.println(saludo.generarSaludo("Mundo"));
-        System.out.println("version 1.0.0 ista para producciòn");
+        
 
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
         
     }
 }
